50c87dd487bd15c298efe62474d792ad6b3d3f2c
rtpearl0319 <121587948+rtpearl0319@users.noreply.github.com>
2026-02-11 11:02:16 -0600

    updated port

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"strconv"
 
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/static"
@@ -13,53 +11,10 @@ func main() {
 
 	app := fiber.New()
 
-	app.Get("/", func(c fiber.Ctx) error {
-		userName := c.Req().Query("userName")
-		userAge := c.Req().Query("userAge")
-
-		return c.SendString("Hello World!" + " " + userName + " " + userAge)
+	app.Get("/", func(c fiber.Ctx) {
+		app.Get("/index", static.New("index.html"))
+		app.Get("/styles.css", static.New("styles.css"))
 	})
 
-	// 1) [x] Create route path called invokeCallback, Post
-	// 2) [x] Move invokeCallbackFunction to GO
-	// 3) [x] Call invokeCallbackFunction when path is used, be sure to utilize the 3 different ways for the params
-
-	app.Post("/invokeCallback", func(c fiber.Ctx) error {
-		// TODO: Call invokeCallbackFunction
-
-		start := func() string {
-			return "I DON'T KNOW MAN"
-		}
-
-		invokeCallBackFunction(start, iHateThis, func(index int) string {
-			fmt.Println("Camden Made Me Do This")
-			return "Completed" + strconv.Itoa(index) + " many times"
-		}, 4)
-
-		return c.SendString("Hello World! We created invokeCallbackFunction")
-
-	})
-
-	app.Get("/index", static.New("index.html"))
-	app.Get("/styles.css", static.New("styles.css"))
-
-	log.Fatal(app.Listen(":3000"))
-}
-
-/*func exampleCallback(onStart func()) {
-	onStart()
-}*/
-
-func iHateThis(index int) {
-	fmt.Println(strconv.Itoa(index) + " Internal Screaming")
-}
-
-func invokeCallBackFunction(start func() string, iHateThis func(int), onComplete func(int) string, indexCounter int) {
-
-	fmt.Println(start())
-
-	for i := 0; i < indexCounter; i++ {
-		iHateThis(i)
-	}
-	onComplete(indexCounter)
+	log.Fatal(app.Listen(":80"))
 }
